bb8648c712a7dab7cc6141603229a2dd5143bd92
chewycinn <hzlnmacaraeg@gmail.com>
2025-04-18 19:28:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function openLetter() {
+    document.querySelector(".letter-content").classList.remove("hidden");
+    document.getElementById("opening-audio").play();
+}
 
+function setMessage() {
+    const message = document.getElementById("message-input").value;
+    document.getElementById("personal-message").textContent = message;
+}
